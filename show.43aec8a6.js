3f6abaf01ec39fe21bd893a578505a9250d7d34b
Dannywei0914 <a0932819576@gmail.com>
2020-06-26 20:28:34 +0800

    edit css

--- show.43aec8a6.js
+++ show.43aec8a6.js
@@ -14307,7 +14307,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61183" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56833" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
